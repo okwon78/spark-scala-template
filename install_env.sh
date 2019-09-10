300eba2d7622c34d216939331a72a5319f91c8e5
obkwon <obkwon@amorepacific.com>
2019-09-10 14:58:03 +0900

    add install shell

---
+++ install_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget http://dl.bintray.com/sbt/rpm/sbt-0.13.8.rpm
+sudo yum install sbt-0.13.8.rpm
+
+wget https://downloads.lightbend.com/scala/2.12.8/scala-2.12.8.rpm
+sudo yum install scala-2.12.8.rpm
